bb2df9f7effddefff079bd59fd5ba44795e188f0
shandli123 <shandlikamal248@gmail.com>
2024-09-06 16:37:31 +0530

    kk

--- src/scripts/greet.sh
+++ src/scripts/greet.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 # This example uses envsubst to support variable substitution in the string parameter type.
 # https://circleci.com/docs/orbs-best-practices/#accepting-parameters-as-strings-or-environment-variables
-export MY_ENV_VAR="Hello, World!" >> "$BASH_ENV"
-export ANOTHER_VAR="42"  >> "$BASH_ENV"
-export RANDOM_VAR="foo-bar" >> "$BASH_ENV"
+#!/bin/bash
+
+# Combine multiple exports in a single block and redirect to $BASH_ENV once
+{
+  echo 'export MY_ENV_VAR="Hello, World!"'
+  echo 'export ANOTHER_VAR="42"'
+  echo 'export RANDOM_VAR="foo-bar"'
+} >> "$BASH_ENV"
+
+# Source the BASH_ENV file to load the environment variables
+# Adding a directive to ignore SC1090 for non-constant source
+# shellcheck source=/dev/null
 source "$BASH_ENV"
