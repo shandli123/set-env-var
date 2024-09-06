aac3e6e0675524ddb3c9f0124330f2ada86c01d6
shandli123 <shandlikamal248@gmail.com>
2024-09-06 16:05:28 +0530

    test

--- src/scripts/greet.sh
+++ src/scripts/greet.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # This example uses envsubst to support variable substitution in the string parameter type.
 # https://circleci.com/docs/orbs-best-practices/#accepting-parameters-as-strings-or-environment-variables
-TO=$(circleci env subst "${PARAM_TO}")
-# If for any reason the TO variable is not set, default to "World"
-echo "Hello ${TO:-World}!"
+ echo "export MY_ENV_VAR='Hello, World!'" >> $BASH_ENV
+echo "export ANOTHER_VAR='42'" >> $BASH_ENV
+echo "export RANDOM_VAR='foo-bar'" >> $BASH_ENV
+# Reload environment variables
+source $BASH_ENV
