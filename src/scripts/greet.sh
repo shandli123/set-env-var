7f0fb187368e968bc27d8e085286070d12ee9d18
shandli123 <shandlikamal248@gmail.com>
2024-09-06 16:24:03 +0530

    logic-fix

--- src/scripts/greet.sh
+++ src/scripts/greet.sh
@@ -2,6 +2,6 @@
 # This example uses envsubst to support variable substitution in the string parameter type.
 # https://circleci.com/docs/orbs-best-practices/#accepting-parameters-as-strings-or-environment-variables
 export MY_ENV_VAR="Hello, World!"
-export ANOTHER_VAR= "42"
-export RANDOM_VAR= "foo-bar"
-source $BASH_ENV
+export ANOTHER_VAR="42"
+export RANDOM_VAR="foo-bar"
+source "$BASH_ENV"
